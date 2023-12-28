f0e5d8e6f2d3af17b73b2be4035f85da8adb50e1
Samuele <samuele8@gmail.com>
2023-12-28 19:08:23 +0100

    d17p2

--- 17/p2.py
+++ 17/p2.py
@@ -19,18 +19,21 @@ with open(sys.argv[1]) as f:
       continue
     visited.add((x, y, dx, dy, steps))
     
-    if x == len(matrix[0]) - 1 and y == len(matrix) - 1:
-      print(f"min heat loss: {heat_loss}")
+    if x == len(matrix[0]) - 1 and y == len(matrix) - 1 and steps >= 4:
+      print(f"min heat loss: {heat_loss} with last steps: {steps}")
       break
 
-    # only consider the current direction if we haven't taken 3 steps yet
-    if steps < 3:
+    # only consider the current direction if we haven't taken 10 steps yet
+    if steps < 10:
       nx = x + dx
       ny = y + dy
 
       if 0 <= nx < len(matrix[0]) and 0 <= ny < len(matrix):
         nhl = heat_loss + matrix[ny][nx]
         heappush(q, (nhl, nx, ny, dx, dy, steps+1))
+
+    if steps < 4 and (dx, dy) != (0, 0):
+      continue
 
     for ndx, ndy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
       # skip current (already done if applicable) and opposite (invalid) dirs
